5d9bd3dd5b76c3195f906f27b6bbfb8374aa214b
Noam Chen <noamchn75@gmail.com>
2020-08-04 22:40:58 +0300

    Add operators

--- Graph.cpp
+++ Graph.cpp
@@ -93,4 +93,71 @@ Graph Graph::operator+(Graph& graph){
     return result;
 }
 
+Graph Graph::operator^(Graph& graph) {
+    std::set<std::string> inter_v = this->v;
+    std::set<std::pair<std::string, std::string>> inter_e = this->e;
+    for(auto it = graph.v.begin(); it != graph.v.end(); it++){
+        if(v.count(*it)){
+            inter_v.insert(*it);
+        }
+    }
+    for(auto it = graph.e.begin(); it != graph.e.end(); it++){
+        if(e.count(*it)){
+            inter_e.insert(*it);
+        }
+    }
+    Graph result(inter_v,inter_e);
+    return result;
+}
+
+Graph Graph::operator-(Graph& graph){
+    std::set<std::string> diff_v = this->v;
+    std::set<std::pair<std::string, std::string>> diff_e = this->e;
+    for(auto it = v.begin(); it != v.end(); it++){
+        if(graph.v.count(*it)){
+            diff_v.erase(*it);
+        }
+    }
+    for(auto it = e.begin(); it != e.end(); it++){
+        if(graph.e.count(*it)){
+            diff_e.erase(*it);
+        }
+    }
+    Graph result(diff_v, diff_e);
+    return result;
+}
+
+Graph Graph::operator*(Graph& graph) {
+    std::set<std::string> product_v;
+    std::set<std::pair<std::string, std::string>> product_e;
+    for(auto it = v.begin(); it != v.end(); it++){
+        for(auto it2 = graph.v.begin(); it2 != graph.v.end(); it2++){
+            std::string str = "[";
+            str.append(*it);
+            str.append(";");
+            str.append(*it2);
+            str.append("]");
+        }
+    }
+    for(auto it = e.begin(); it != e.end(); it++){
+        for(auto it2 = graph.e.begin(); it2 != graph.e.end(); it2++) {
+            std::string str1 = "[";
+            str1.append(it->first);
+            str1.append(";");
+            str1.append(it2->first);
+            str1.append("]");
+            std::string str2 = "[";
+            str2.append(it->second);
+            str2.append(";");
+            str2.append(it2->second);
+            str2.append("]");
+            std::pair<std::string, std::string> result(str1, str2);
+            product_e.insert(result);
+        }
+    }
+    Graph product(product_v, product_e);
+    return product;
+}
+
+
 
